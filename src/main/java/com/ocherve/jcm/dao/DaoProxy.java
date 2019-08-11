4f6d9a7e25565b4bc9d56f906dc12fcbec97b5ba
hervep530 <hervep6963@gmail.com>
2019-08-11 09:31:24 +0200

    Remodeling architecture... In DaoProxy removing all SessionDao references and Adding : UserDao SiteDao TopoDao CommentDao MessageDao

--- src/main/java/com/ocherve/jcm/dao/DaoProxy.java
+++ src/main/java/com/ocherve/jcm/dao/DaoProxy.java
@@ -5,8 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import com.ocherve.jcm.dao.contract.Dao;
-import com.ocherve.jcm.dao.contract.SessionDao;
+import com.ocherve.jcm.dao.contract.*;
 import com.ocherve.jcm.dao.impl.DaoFactory;
 import com.ocherve.jcm.utils.PropertiesHelper;
 
@@ -20,7 +19,11 @@ public class DaoProxy {
     protected static final Logger DLOG = LogManager.getLogger("development_file");
 
     private static StorageType daoType;
-	private Dao sessionDao;
+	private Dao userDao;
+	private Dao siteDao;
+	private Dao topoDao;
+	private Dao messageDao;
+	private Dao commentDao;
 	private static DaoProxy instance = new DaoProxy();
 	
 	private DaoProxy() {
@@ -29,7 +32,11 @@ public class DaoProxy {
 		String daoConfig = PropertiesHelper.getConfigValue("daoType");
 		if ( daoConfig == null ) throw new RuntimeException();
 		daoType = StorageType.valueOf(daoConfig.toUpperCase());
-		this.sessionDao = DaoFactory.getDao(daoType, SessionDao.class);
+		this.userDao = DaoFactory.getDao(daoType, UserDao.class);
+		this.siteDao = DaoFactory.getDao(daoType, SiteDao.class);
+		this.topoDao = DaoFactory.getDao(daoType, TopoDao.class);
+		this.messageDao = DaoFactory.getDao(daoType, MessageDao.class);
+		this.commentDao = DaoFactory.getDao(daoType, CommentDao.class);
 	}
 
 	/**
@@ -46,12 +53,47 @@ public class DaoProxy {
 	/**
 	 * Getter
 	 * 
-	 * @return		Dao : SessionDaoPgImpl under generic form
+	 * @return the userDao
 	 */
-	public Dao getSessionDao() {
-		return sessionDao;
+	public Dao getUserDao() {
+		return userDao;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return the siteDao
+	 */
+	public Dao getSiteDao() {
+		return siteDao;
+	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return the topoDao
+	 */
+	public Dao getTopoDao() {
+		return topoDao;
+	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return the messageDao
+	 */
+	public Dao getMessageDao() {
+		return messageDao;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the commentDao
+	 */
+	public Dao getCommentDao() {
+		return commentDao;
+	}
+	
+	
 }
