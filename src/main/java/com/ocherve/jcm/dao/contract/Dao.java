add89fb823085134e48f2ef3266164fffec3bbe5
hervep530 <hervep6963@gmail.com>
2019-08-11 09:34:33 +0200

    Adding Crud basic methods in Dao interface (Done before specific Dao but not yet commited)

--- src/main/java/com/ocherve/jcm/dao/contract/Dao.java
+++ src/main/java/com/ocherve/jcm/dao/contract/Dao.java
@@ -7,4 +7,25 @@ package com.ocherve.jcm.dao.contract;
  */
 public interface Dao {
 
+	/**
+	 * @param entityClass
+	 * @param object
+	 * @return entity manager by dao
+	 */
+	Object create(Class<?> entityClass, Object object);
+	
+	/**
+	 * @param entityClass
+	 * @param id
+	 * @return entity manager by dao
+	 */
+	Object get(Class<?> entityClass, Integer id);
+	
+	/**
+	 * @param entityClass
+	 * @param id
+	 * @return true if deleted, false if not deleted
+	 */
+	boolean delete(Class<?> entityClass, Integer id);
+	
 }
