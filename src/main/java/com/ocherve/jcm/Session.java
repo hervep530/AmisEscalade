56fc7795c5861c02bd52d54f84d1ab4742eb0d45
hervep530 <hervep6963@gmail.com>
2019-09-21 11:08:58 +0200

    ## Using Notification managent in Session component In src/main/java/com/ocherve/jcm/Session.java : - extending JcmServlet instead of HttpServlet - adding notification management - remove error jsp forward in doPost (error are displayed with refreshing formular) - Using servlet variable define by JcmServlet - Adding Comment In src/main/java/com/ocherve/jcm/service/impl/SessionServiceImpl.java : - Adding postConnexionForm(...) and postInscriptionForm(...) - Calling this method in switch case of doPostAction() - Adding notification management in doGetAction()

--- src/main/java/com/ocherve/jcm/Session.java
+++ src/main/java/com/ocherve/jcm/Session.java
@@ -6,52 +6,57 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.ocherve.jcm.service.Delivry;
-import com.ocherve.jcm.service.Parameters;
 import com.ocherve.jcm.service.ServiceException;
 import com.ocherve.jcm.service.ServiceProxy;
+import com.ocherve.jcm.service.factory.SessionService;
 
 /**
  * Servlet implementation class Session
  */
 @WebServlet("/session/*")
-public class Session extends HttpServlet {
+public class Session extends JcmServlet {
 
 	private static final long serialVersionUID = 1L;
        
 	private static final String VUE = "/WEB-INF/SessionLayout.jsp";
 	private static final String PAGE_ERROR = "/WEB-INF/ErrorLayout.jsp";
+	
+	// session, service, parameters and delivry variable herited from abstract JcmServlet
 
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Session() {
         super();
-        // TODO Auto-generated constructor stub
+        service = (SessionService) ServiceProxy.getInstance().getSessionService();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// initializing session (heriting jcmServlet)
+		this.startSession(request);
+		// Getting deferred notification from the next http request (was stored in session)
+		request.setAttribute("notifications", this.getSessionNotifications());
 		request.setAttribute("uri", request.getRequestURI());
-		// HttpSession session = request.getSession();
-		Parameters parameters = null;
-		Delivry delivry = null;
 		
+		// we set service parameters from request and execute "GET" action. Result is return with delivry.
 		try {
-			parameters = ServiceProxy.getInstance().getSessionService().setParameters(request);
-			delivry = ServiceProxy.getInstance().getSessionService().doGetAction(parameters);
+			parameters = service.setParameters(request);
+			delivry = service.doGetAction(parameters);
 		} catch (ServiceException e ) {
 			System.out.println("Erreur Servlet Session");
-			delivry = ServiceProxy.getInstance().getSessionService().abort(parameters);
+			delivry = service.abort(parameters);
 		}
-
 		request.setAttribute("delivry", delivry);
-		// session.setAttribute("notification", delivry.getSession().get("notification"));
 
+		// Deferred notification (if exists) copied from delivry to session (heriting jcmServlet)
+		this.setSessionNotification();
+
+		// Forwarding to Session jsp or error
 		if ( delivry.getErrors().isEmpty() )
 			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 		else
@@ -64,30 +69,30 @@ public class Session extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// initializing session (heriting jcmServlet)
+		this.startSession(request);
+
+		// Getting deferred notification from the next http request (was stored in session)
+		request.setAttribute("notifications", this.getSessionNotifications());
 		request.setAttribute("uri", request.getRequestURI());
-		HttpSession session = request.getSession();
-		Parameters parameters = null;
-		Delivry delivry = null;
 		
+		// we set service parameters from request and execute "POST" action. Result is return with delivry.
 		try {
-			parameters = ServiceProxy.getInstance().getSessionService().setParameters(request);
-			delivry = ServiceProxy.getInstance().getSessionService().doPostAction(parameters);
+			parameters = service.setParameters(request);
+			delivry = service.doPostAction(parameters);
 		} catch (ServiceException e ) {
-			System.out.println("============== Erreur Servlet Session ==================");
-			delivry = ServiceProxy.getInstance().getSessionService().abort(parameters);
+			delivry = service.abort(parameters);
 		}
-
 		request.setAttribute("delivry", delivry);
-		// session.setAttribute("notification", delivry.getSession().get("notification"));
-		for ( String attribute : delivry.getSession().keySet()) {
-			if ( ! attribute.contentEquals("notification") ) 
-				session.setAttribute(attribute, delivry.getSession().get(attribute));
-		}
 
-		if ( delivry.getErrors().isEmpty() )
-			response.sendRedirect(this.getServletContext().getContextPath());
-		else
+		// Forwarding to Site jsp, redirect,  or forwarding error
+		if ( delivry.getAttributes().containsKey("redirect") ) {
+			this.setSessionNotification();
+			response.sendRedirect((String) delivry.getAttributes().get("redirect")); 
+		} else {
+			// when posting formular... it means that we can display VUE even if delivry contents errors
 			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		}
 
 	}
 
--- src/main/java/com/ocherve/jcm/service/impl/SessionServiceImpl.java
+++ src/main/java/com/ocherve/jcm/service/impl/SessionServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ocherve.jcm.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.Level;
@@ -8,6 +11,8 @@ import com.ocherve.jcm.model.User;
 import com.ocherve.jcm.form.ConnexionForm;
 import com.ocherve.jcm.form.InscriptionForm;
 import com.ocherve.jcm.service.Delivry;
+import com.ocherve.jcm.service.Notification;
+import com.ocherve.jcm.service.NotificationType;
 import com.ocherve.jcm.service.Parameters;
 import com.ocherve.jcm.service.UrlException;
 import com.ocherve.jcm.service.factory.SessionService;
@@ -69,7 +74,9 @@ public class SessionServiceImpl extends ServiceImpl implements SessionService {
 			DLOG.log(Level.ERROR , e.getMessage());
 			delivry.appendError(serviceName + "_" + parameters.getParsedUrl().getAction(), e.getMessage());
 		}
+		if ( delivry == null ) delivry = new Delivry();
 		delivry.setParameters(parameters);
+		if ( ! parameters.getNotifications().isEmpty() ) delivry.appendNotifications(parameters.getNotifications());
 		if ( ! parameters.getErrors().isEmpty() ) delivry.setErrors(parameters.getErrors());
 		String info = "Service " + this.serviceName + " do GetAction.";
 		DLOG.log(Level.DEBUG , info);
@@ -81,22 +88,10 @@ public class SessionServiceImpl extends ServiceImpl implements SessionService {
 		try {
 			switch (parameters.getParsedUrl().getAction()) {
 				case "connexion" :
-					ConnexionForm connexionForm = (ConnexionForm) parameters.getForm();
-					User user = connexionForm.connectUser();
-					if ( ! connexionForm.getErrors().isEmpty() ) {
-						delivry.setErrors(connexionForm.getErrors());
-						delivry.appendattribute("connexionForm", connexionForm);
-					} 
-					delivry.appendSession("sessionUser", user);
+					delivry = postConnexionForm(parameters);
 					break;
 				case "inscription" :
-					InscriptionForm inscriptionForm = (InscriptionForm) parameters.getForm();
-					User inscriptionUser = inscriptionForm.createUser();
-					if ( ! inscriptionForm.getErrors().isEmpty() ) {
-						delivry.setErrors(inscriptionForm.getErrors());
-						delivry.appendattribute("inscriptionForm", inscriptionForm);
-					} 
-					delivry.appendattribute("user", inscriptionUser);
+					delivry = postInscriptionForm(parameters);
 					break;
 				case "pass" :
 					break;
@@ -111,6 +106,60 @@ public class SessionServiceImpl extends ServiceImpl implements SessionService {
 		String info = "Service " + this.serviceName + " do GetAction.";
 		DLOG.log(Level.DEBUG , info);
 		return delivry;
+	}
+	
+	private Delivry postConnexionForm(Parameters parameters) { 
+		Delivry delivry = new Delivry();
+		// get Connexion Form stored in parameters and call connectUser()
+		ConnexionForm connexionForm = (ConnexionForm) parameters.getForm();
+		User user = connexionForm.connectUser();
+		if ( ! connexionForm.getErrors().isEmpty() ) {
+			// if errors, we will forward delivry to formular with errors embedded in connexionForm
+			delivry.setErrors(connexionForm.getErrors());
+			delivry.appendattribute("connexionForm", connexionForm);
+			
+			return delivry;
+		} 
+		
+		// if Success, user is stored in session and we will send in the next httpRequest (redirection)
+		delivry.appendSession("sessionUser", user);
+		// Settin redirection and notification(s)
+		String notificationLabel = "Connexion";
+		String message = "Bonjour " + user.getUsername() + "! Content de vous revoir.";
+		Notification notification = new Notification(NotificationType.SUCCESS, message);
+		Map<String,Notification> notifications = new HashMap<>();
+		notifications.put(notificationLabel, notification);
+		delivry.appendSession("notifications", notifications);
+		delivry.appendattribute("redirect", parameters.getContextPath());
+		
+		return delivry;
+	}
+	
+	private Delivry postInscriptionForm(Parameters parameters) {
+		Delivry delivry = new Delivry();
+
+		// get Connexion Form stored in parameters and call connectUser()
+		InscriptionForm inscriptionForm = (InscriptionForm) parameters.getForm();
+		User inscriptionUser = inscriptionForm.createUser();
+		if ( ! inscriptionForm.getErrors().isEmpty() ) {
+			// if errors, we will forward delivry to formular with errors embedded in connexionForm
+			delivry.setErrors(inscriptionForm.getErrors());
+			delivry.appendattribute("inscriptionForm", inscriptionForm);
+			
+			return delivry;
+		} 
+		
+		// Setting redirection and notification(s)
+		String notificationLabel = "Creation de compte";
+		String message = "Votre compte est créé. Vous pouvez maintenant vous connecter.";
+		Notification notification = new Notification(NotificationType.SUCCESS, message);
+		Map<String,Notification> notifications = new HashMap<>();
+		notifications.put(notificationLabel, notification);
+		delivry.appendSession("notifications", notifications);
+		delivry.appendattribute("redirect", parameters.getContextPath() + "/session/connexion");
+		
+		return delivry;
+		
 	}
 	
 }
