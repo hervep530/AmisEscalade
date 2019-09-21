3b63e30450194a81df1864cd202398d48dea26f3
hervep530 <hervep6963@gmail.com>
2019-09-21 10:51:49 +0200

    ## Extending Default Servlet from JcmServlet In src/main/java/com/ocherve/jcm/Default.java : - Modifying extends - Adding notification management - Using servlet variable define by JcmServlet - Adding Comment

--- src/main/java/com/ocherve/jcm/Default.java
+++ src/main/java/com/ocherve/jcm/Default.java
@@ -7,28 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ocherve.jcm.service.Delivry;
-import com.ocherve.jcm.service.Parameters;
 import com.ocherve.jcm.service.ServiceException;
 import com.ocherve.jcm.service.ServiceProxy;
+import com.ocherve.jcm.service.factory.DefaultService;
 
 /**
  * Servlet implementation class Default
  */
 @WebServlet("/")
-public class Default extends HttpServlet {
+public class Default extends JcmServlet {
 	
 	private static final long serialVersionUID = 1L;
        
 	private static final String VUE = "/WEB-INF/DefaultLayout.jsp";
 	private static final String PAGE_ERROR = "/WEB-INF/ErrorLayout.jsp";
+	
+	// session, service, parameters and delivry variable herited from abstract JcmServlet
 
 	/**
      * @see HttpServlet#HttpServlet()
      */
     public Default() {
         super();
-        // TODO Auto-generated constructor stub
+        service = (DefaultService) ServiceProxy.getInstance().getDefaultService();
     }
 
 	/**
@@ -36,19 +37,22 @@ public class Default extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// initializing session (heriting jcmServlet)
+		this.startSession(request);
+		// Getting deferred notification from the next http request (was stored in session)
+		request.setAttribute("notifications", this.getSessionNotifications());
 		request.setAttribute("uri", request.getRequestURI());
-		Parameters parameters = null;
-		Delivry delivry = null;
-		
+				
+		// we set service parameters from request and execute "GET" action. Result is return with delivry.
 		try {		
-			parameters = ServiceProxy.getInstance().getDefaultService().setParameters(request);
-			delivry = ServiceProxy.getInstance().getDefaultService().doGetAction(parameters);
+			parameters = service.setParameters(request);
+			delivry = service.doGetAction(parameters);
 		} catch (ServiceException e) {
-			delivry = ServiceProxy.getInstance().getDefaultService().abort(parameters);
+			delivry = service.abort(parameters);
 		}
-
 		request.setAttribute("delivry", delivry);
 
+		// we set service parameters from request and execute "GET" action. Result is return with delivry.		
 		if ( delivry.getErrors().isEmpty() )
 			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 		else
